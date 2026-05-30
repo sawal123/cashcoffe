192c100ab6c2c92d2850d14712d9834d97d06a50
sawal123 <sawalinto@gmail.com>
2026-05-30 14:27:00 +0700

    refactor: update providers registration to ensure class existence and improve readability

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
-return [
+return array_filter([
     App\Providers\AppServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
-];
+    class_exists(Barryvdh\DomPDF\ServiceProvider::class) ? Barryvdh\DomPDF\ServiceProvider::class : null,
+]);
--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Forms\LoginForm;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
